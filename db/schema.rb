46695465654d8160126a0c8f3b5d28a6df6b266d
hugo.alves <goninja@gmail.com>
2015-01-15 18:02:10 +0000

    com nome para os users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115165534) do
+ActiveRecord::Schema.define(version: 20150115171403) do
 
   create_table "buzzwords", force: true do |t|
     t.string   "title"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20150115165534) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

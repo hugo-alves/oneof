8d4c8ccdb96f4917b668275992330e42a082eb45
hugo.alves <goninja@gmail.com>
2015-01-15 11:03:27 +0000

    com o titleize na buzzword

--- app/models/buzzword.rb
+++ app/models/buzzword.rb
@@ -1,3 +1,12 @@
 class Buzzword < ActiveRecord::Base
 	belongs_to :user
+
+	before_save :tileize_title
+  	before_create :tileize_title
+
+	def tileize_title
+		self.title = self.title.titleize
+	end
+
+
 end

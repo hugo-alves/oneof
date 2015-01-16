6698044926f505df3c378342570d865cee478291
hugo.alves <goninja@gmail.com>
2015-01-16 15:25:34 +0000

    com o mandril a mandar mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,13 +59,15 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = {:host => "oneofthosewords.com"}
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
     :port      => 587,
+    :authentication => "plain",
+    :enable_starttls_auto => true,
     :user_name => ENV["MANDRILL_USERNAME"],
     :password  => ENV["MANDRILL_APIKEY"]
-  }
+    }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true

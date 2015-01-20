8cd467e597102758139fd070e4f084fa629cb706
hugo.alves <goninja@gmail.com>
2015-01-20 12:27:24 +0000

    com karma nos users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   acts_as_voter
+  has_karma :buzzwords, :as => :user, :weight => [ 1, 0.5 ]
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
